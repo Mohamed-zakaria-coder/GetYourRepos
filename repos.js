2bfa54e6115d6f601a5b46d79f0442e5a2491b15
Mohamed Z <mz7891555@gmail.com>
2023-03-21 14:28:00 +0200

    add code link

--- repos.js
+++ repos.js
@@ -10,6 +10,7 @@ submitBtn.addEventListener("click", () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        console.log(data.html_url)
         fetch(data.repos_url)
           .then((res) => res.json())
           .then((eachRepo) => {
@@ -21,6 +22,10 @@ submitBtn.addEventListener("click", () => {
               link.textContent = `Link`;
               link.target = "_blank";
               link.href = `https://${repo.owner.login}.github.io/${repo.name}`;
+              let codeContent = document.createElement('a');
+              codeContent.href = `${data.html_url}/${repo.name}` 
+              codeContent.textContent = "Code"
+              codeContent.target = "_blank"
               let eachRepoContainer = document.createElement("div");
               let infoContainer = document.createElement("div");
               let eachRepo = document.createElement("div");
@@ -29,9 +34,10 @@ submitBtn.addEventListener("click", () => {
               eachRepo.appendChild(h3);
               infoContainer.appendChild(link);
               infoContainer.appendChild(projectStars);
+              infoContainer.appendChild(codeContent)
               infoContainer.className = "info-container";
               infoContainer.style.cssText =
-                "display:flex; justify-content:space-between; align-items:center";
+                "display:flex; justify-content:space-around; align-items:center";
               reposContent.appendChild(eachRepoContainer);
               eachRepoContainer.appendChild(eachRepo);
               eachRepoContainer.appendChild(infoContainer);
